2bdc318bc7430ba0dda6d0db3e0bfc342f933c0e
CodingAraiguma <neilhelber1@hotmail.com>
2025-08-09 23:27:03 +0200

    Update api.config.ts

--- src/api.config.ts
+++ src/api.config.ts
@@ -3,7 +3,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 export const API_CONFIG = {
   BASE_URL: isDevelopment 
     ? '/api' // Will be proxied to your backend in development
-    : 'https://your-production-api-url.com', // Replace with your production API URL
+    : 'https://api.wolf0fdev.me/notes/api/v1/', // Replace with your production API URL
   
   ENDPOINTS: {
     USER_CONFIG: '/user/config',
